c65582bcd083581f9315a5916d32d7ccd4b6f7a5
Your Name <anjalishikhare755@gmail.com>
2026-03-04 11:17:07 +0530

    Fixed scanner warning issue

--- MainATM.java
+++ MainATM.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class MainATM {
+    @SuppressWarnings("resource")
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -13,7 +14,7 @@ public class MainATM {
         System.out.print("Enter PIN: ");
         int pin = sc.nextInt();
 
-        sc.close();
+        
 
         if (user.validate(uid, pin)) {
             int choice;
